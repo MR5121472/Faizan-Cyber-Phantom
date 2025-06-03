f5868e66d30b94b0d5347a90646265a403afd148
MR5121472 <177574924+MR5121472@users.noreply.github.com>
2025-06-03 08:16:13 +0500

    proxy_engine.py

--- core/proxy_engine.py
+++ core/proxy_engine.py
@@ -1,23 +1,22 @@
-
-import time
+import socket
+import socks
 
 class ProxyEngine:
     def __init__(self):
-        self.ips = ["185.32.1.11", "192.168.44.1", "104.21.77.245", "172.217.14.206"]
+        print("[+] Initializing Faizan™ Privacy Proxy Engine...")
 
     def start_proxy(self):
-        print("[+] Initializing Faizan™ Privacy Proxy Engine...")
-        print("[+] Proxy Engine started in Lite Mode (No external libs)\n")
-        self.rotate_ips()
+        print("[+] Proxy Engine started in Lite Mode (No external libs)")
 
-    def rotate_ips(self):
-        print("[*] Starting IP rotation simulation every 2 seconds...\n")
-        for ip in self.ips:
-            print(f"[✔] IP changed to: {ip}")
-            time.sleep(2)
-        print("\n[✓] IP rotation simulation complete.")
+    def connect(self, host, port):
+        try:
+            socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9050)
+            socket.socket = socks.socksocket
 
-# For direct testing (optional)
-if __name__ == "__main__":
-    engine = ProxyEngine()
-    engine.start_proxy()
+            s = socket.socket()
+            s.connect((host, port))
+            s.close()
+            return True
+        except Exception as e:
+            print(f"[-] Connection error: {e}")
+            return False
